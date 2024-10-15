9a90e14d6d1e0bdf13c214c6921ecb7ab9c45e0e
sunny kumar <sunnykr3913@gmail.com>
2024-10-15 12:41:06 +0530

    first commit

---
+++ src/interfaces.js
@@ -0,0 +1,26 @@
+// // Ticket structure
+// const Ticket = {
+//   id: "", // string
+//   title: "", // string
+//   tag: [], // Array of strings
+//   userId: "", // string
+//   status: "", // string
+//   priority: 0, // number
+// };
+
+// // User structure
+// const User = {
+//   id: "", // string
+//   name: "", // string
+//   available: false, // boolean
+// };
+
+// // Column structure for holding tickets
+// const Col = {
+//   col: [], // Array of Ticket objects
+// };
+
+// // UserIdToData structure for mapping user IDs to User data
+// const UserIdToData = {
+//   userData: {}, // Record of user ID to User objects
+// };
