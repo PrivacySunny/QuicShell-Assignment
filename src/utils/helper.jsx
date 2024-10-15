b8ce5fa5a228b5aa41542189e34cc5edca1dfbbc
sunny kumar <sunnykr3913@gmail.com>
2024-10-15 13:40:16 +0530

    change icon

--- src/utils/helper.jsx
+++ src/utils/helper.jsx
@@ -1,9 +1,11 @@
-import { BiRadioCircle } from "react-icons/bi";
 import { LuMoreHorizontal } from "react-icons/lu";
-import { TbProgress } from "react-icons/tb";
 import { IoCheckmarkDoneCircle } from "react-icons/io5";
 import { AiFillCloseCircle, AiFillWarning } from "react-icons/ai";
 import { BiSignal2, BiSignal3, BiSignal4 } from "react-icons/bi";
+import { RiProgress8Line } from "react-icons/ri";
+import { TbCircleDashed } from "react-icons/tb";
+import { FaRegCircle } from "react-icons/fa";
+
 
 export const getPriorityIcon = (priority) => {
   switch (priority) {
@@ -25,15 +27,15 @@ export const getPriorityIcon = (priority) => {
 export const getStatusIcon = (status) => {
   switch (status) {
     case "Backlog":
-      return <BiRadioCircle color="#e2e2e2" size={24} />;
+      return <TbCircleDashed color="#686c6e" size={18} />;
     case "Todo":
-      return <BiRadioCircle color="#e2e2e2" size={24} />;
+      return <FaRegCircle color="##686c6e" size={18} />;
     case "In progress":
-      return <TbProgress color="#f1ca4b" size={16} />;
+      return <RiProgress8Line color="#deae10" size={20} />;
     case "Done":
-      return <IoCheckmarkDoneCircle color="#5e6ad2" size={16} />;
+      return <IoCheckmarkDoneCircle color="#4a61eb" size={16} />;
     case "Canceled":
-      return <AiFillCloseCircle color="#94a2b3" size={16} />;
+      return <AiFillCloseCircle color="#9da9f0" size={16} />;
     default:
       return <AiFillCloseCircle color="#94a2b3" size={16} />;
   }
